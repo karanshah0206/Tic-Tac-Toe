3e5808f35aade1e194142b7712c28f1c21d578bc
Karan Shah <karanshah0206@outlook.com>
2021-06-30 14:00:16 +0530

    Sending UI On Get Request

--- server.js
+++ server.js
@@ -7,3 +7,13 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 server.listen(port, () => { });
+
+// Serve Frontend
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/ui/");
+});
+
+// Connection Management
+io.on("connection", (socket) => {
+    console.log("User connected on socket " + socket);
+});
