0d91b4f01cc50866f113c509bb0c153caa9a4157
Karan Shah <karanshah0206@outlook.com>
2021-06-30 13:52:24 +0530

    Added socket.io

--- server.js
+++ server.js
@@ -2,4 +2,8 @@
 const port = 80;
 const express = require("express");
 const app = express();
-app.listen(port);
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+server.listen(port, () => { });
